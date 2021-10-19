a80a2bfcbca930f333e2c615bafc1f56b2759d41
Nhlez <notrealmail@notrealmail.ps.com.ps>
2021-10-19 23:05:30 +0200

    Cleaning my repository

--- Guardar movimientos del mouse.py
+++ Guardar movimientos del mouse.py
@@ -1,20 +1,18 @@
-import pyautogui, time,random
+import pyautogui
 from pynput.mouse import Listener
 import keyboard as key2
 from pynput import mouse
-
-
+# Fijarse si discrimina entre click derecho e izquierdo
 import time  
 
-
 clicks_guardados = []
-time_each_click = [3,]
+time_each_click = []
 time_between_clicks = []
 tiempo_list = []
 
-
-
 new_list = []
+
+# With this function we clean the output string from Pyautogui.
 def replace_and_add_item(items,old1,new1,old2,new2,old3,new3):
     new_item = items.replace(old1, new1)
     new_item = new_item.replace(old2, new2)
@@ -38,50 +36,42 @@ def on_click(x, y, button, pressed):
         tiempo_list.append(time.time())
         print(tiempo_list)
         print(pyautogui.position())
-        print('se guardo un click')
+        print('A click was saved')
         print(clicks_guardados)
     
-    
+# ------------------Start program-----------------------------------------------
 n = True        
 while n == True:
     with Listener(on_click=on_click) as listener:
         listener.join()
 
+#Esta linea corta el while 
     if str(pyautogui.position()) == 'Point(x=0, y=0)':
         n = False
     
     
 diff_list = []
+# El bucle for de acontinuacion nos genera el intervalo final de tiempo.
 for i in range(1,len(tiempo_list)):
     if tiempo_list[i] != tiempo_list[0]:
         x = tiempo_list[i] - tiempo_list[i-1]
         diff_list.append(x)
-
+diff_list.insert(0, 1)
 print(diff_list)
-
-#while True:
-       #elif keyboard.is_pressed('x'):
-        #mensaje = ' aca va algo del teclado'
-        #clicks_guardados.append(str(pyautogui.position())+mensaje)
-        #print(mensaje)
-        #time.sleep(0.5)
-    #else:
-        #continue
-
 
 for i in clicks_guardados:
     replace_and_add_item(str(i),'Point','','x=','','y=','')
 
 clicks = str(new_list).replace("'", "")
 print(new_list)
-print('Se ha procesado la lista satisfactoriamente')
-decision = input('¿Deseas guardarla ? press y/n')
+print('The list has been proccessed succesfully')
+decision = input('¿Do you want to save it? press y/n')
 
 if decision == 'y':
-    file_name = input('Choose the filename: ')+'.txt'
+    file_name = input('Choose the filename: ')+'.json'
     file_clicks = open(file_name,'w+')
-    file_clicks.write(clicks)
+    file_clicks.write(clicks+'\n')
+    file_clicks.write(str(diff_list))
     file_clicks.close()
-print(new_list)
 
-input()
+
