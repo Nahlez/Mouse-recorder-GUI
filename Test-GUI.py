247d5b7cfef4f8dd4ccb871cb16b6b9ff5b7e74f
Nhlez <notrealmail@notrealmail.ps.com.ps>
2021-10-26 12:26:34 +0200

    The script can open a generated json with the coordinates and time and run the macro

--- Test-GUI.py
+++ Test-GUI.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from tkinter import filedialog
 import os
+import pyautogui
+import time
+import keyboard
+
+
 
 root = Tk()
 
@@ -8,14 +13,27 @@ root.geometry('300x300')
 
 def open_json():
     json_instructions = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(("Json File", "*.json"), ("All Files", "*.*")),
-                           title="Choose a file."
-                           )
+                           title="Choose a file.")
     json_instructions = open(json_instructions,'r')
     lines = json_instructions.readlines()
-    clicks_coordenates = lines[0]
-    time_clicks = lines[1]
+    open_json.clicks_coordenates = eval(lines[0])
+    open_json.time_clicks = eval(lines[1])
 
-    print(clicks_coordenates, time_clicks)
-
+    
 open_json()
+
+# Here starts the script "Macro play test".
+
+saved_clicks = open_json.clicks_coordenates
+time_between_clicks = open_json.time_clicks
+
+time.sleep(0.8)
+index = 0
+for i in saved_clicks:
+    time.sleep(time_between_clicks[index]) 
+    index += 1 
+    pyautogui.click(i)
+
+
+
 root.mainloop()
