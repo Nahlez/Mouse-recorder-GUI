7dbef732489c77c679b286b71b187a8c9bd6ef99
Nhlez <notrealmail@notrealmail.ps.com.ps>
2021-10-26 14:30:46 +0200

    I have added two buttons. One to open the JSON and one to execute the macro.

--- Test-GUI.py
+++ Test-GUI.py
@@ -11,6 +11,8 @@ root = Tk()
 
 root.geometry('300x300')
 
+
+
 def open_json():
     json_instructions = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(("Json File", "*.json"), ("All Files", "*.*")),
                            title="Choose a file.")
@@ -19,20 +21,26 @@ def open_json():
     open_json.clicks_coordenates = eval(lines[0])
     open_json.time_clicks = eval(lines[1])
 
+def macro_play():
+    saved_clicks = open_json.clicks_coordenates
+    time_between_clicks = open_json.time_clicks
+
+    time.sleep(0.8)
+    index = 0
+    for i in saved_clicks:
+        time.sleep(time_between_clicks[index]) 
+        index += 1 
+        pyautogui.click(i)
+
+button = Button(root, text='Open file', command=open_json).pack()
+button_run = Button(root, text='Run', command=macro_play).pack()
+
+
+
+
     
-open_json()
 
 # Here starts the script "Macro play test".
-
-saved_clicks = open_json.clicks_coordenates
-time_between_clicks = open_json.time_clicks
-
-time.sleep(0.8)
-index = 0
-for i in saved_clicks:
-    time.sleep(time_between_clicks[index]) 
-    index += 1 
-    pyautogui.click(i)
 
 
 
